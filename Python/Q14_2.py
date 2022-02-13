97cd196ba6f1267b267dc41894b7070531d53c60
Mushi0 <zhy_980401@outlook.com>
2022-02-13 13:04:15 +0000

    Third method for Q14_2

--- Python/Q14_2.py
+++ Python/Q14_2.py
@@ -11,7 +11,7 @@
 # pair_rules = polymer_pairs[2:]
 # pair_rules = dict([rule.split(' -> ') for rule in pair_rules])
 #
-# # 10 steps
+# # 40 steps
 # for i in range(40):
 #     start = datetime.now()
 #     # find all the insertions
@@ -32,27 +32,70 @@
 
 #====================================================================
 
-import numpy as np
+# import numpy as np
+#
+# def insert(nb_steps, polymer):
+#     '''
+#     Recursively count the appearance of elements in the polymer
+#
+#     Input: nb_steps (int): the number of steps
+#             polymer (str): the original polymer (2 chars)
+#     Output: the count numbers in the order of alphabet
+#     '''
+#     # if this is the last step, return the count
+#     if nb_steps == 0:
+#         result = np.zeros(np_alph)
+#         result[alphabet.index(polymer[0])] += 1
+#         return result
+#     # else, insert the new element and return the sum of the counts
+#     else:
+#         nb_steps -= 1
+#         insertion = pair_rules[polymer]
+#         result = insert(nb_steps, polymer[0] + insertion) + insert(nb_steps, insertion + polymer[1])
+#         return result
+#
+# # read data from file
+# TXT_FILE = 'Data/Q14.txt'
+# with open(TXT_FILE) as f:
+#     polymer_pairs = f.read()
+# # tidy up the data, create the polymer string and list of rules
+# polymer_pairs = polymer_pairs.split('\n')[:-1]
+# polymer = polymer_pairs[0]
+# pair_rules = polymer_pairs[2:]
+# pair_rules = dict([rule.split(' -> ') for rule in pair_rules])
+# # the set of all the elemnts appear in the polymer
+# alphabet = list(set(pair_rules.values()))
+# np_alph = len(alphabet)
+#
+# count_polymer = np.zeros(np_alph) # initialize the count
+# # loop theough all the pairs in the polymer
+# for i in range(len(polymer) - 1):
+#     count_polymer += insert(40, polymer[i:i + 2])
+# count_polymer[alphabet.index(polymer[-1])] += 1
+# # the answer
+# print(max(count_polymer) - min(count_polymer))
 
-def insert(nb_steps, polymer):
+#====================================================================
+# Merge sort
+from collections import Counter
+
+def insert(polymer):
     '''
-    Recursively count the appearance of elements in the polymer
+    Splits the string to two substring and recursively insert the element
 
     Input: nb_steps (int): the number of steps
-            polymer (str): the original polymer (2 chars)
-    Output: the count numbers in the order of alphabet
+            polymer (str): the original polymer
+    Output: the result string
     '''
-    # if this is the last step, return the count
-    if nb_steps == 0:
-        result = np.zeros(np_alph)
-        result[alphabet.index(polymer[0])] += 1
-        return result
-    # else, insert the new element and return the sum of the counts
+    len_polymer = len(polymer)
+    # if only one char, return it
+    if len_polymer == 1:
+        return polymer
+    # else, return the two substring
     else:
-        nb_steps -= 1
-        insertion = pair_rules[polymer]
-        result = insert(nb_steps, polymer[0] + insertion) + insert(nb_steps, insertion + polymer[1])
-        return result
+        half_len = len_polymer//2
+        return insert(polymer[:half_len]) + pair_rules[polymer[half_len - 1: half_len + 1]] + \
+                insert(polymer[half_len:])
 
 # read data from file
 TXT_FILE = 'Data/Q14.txt'
@@ -63,16 +106,17 @@ polymer_pairs = polymer_pairs.split('\n')[:-1]
 polymer = polymer_pairs[0]
 pair_rules = polymer_pairs[2:]
 pair_rules = dict([rule.split(' -> ') for rule in pair_rules])
-# the set of all the elemnts appear in the polymer
-alphabet = list(set(pair_rules.values()))
-np_alph = len(alphabet)
 
-count_polymer = np.zeros(np_alph) # initialize the count
-# loop theough all the pairs in the polymer
-for i in range(len(polymer) - 1):
-    count_polymer += insert(40, polymer[i:i + 2])
-count_polymer[alphabet.index(polymer[-1])] += 1
+# polymer = 'NNCB'
+# pair_rules = {'CH': 'B', 'HH': 'N', 'CB': 'H', 'NH': 'C', 'HB': 'C', \
+#             'HC': 'B', 'HN': 'C', 'NN': 'C', 'BH': 'H', 'NC': 'B', \
+#             'NB': 'B', 'BN': 'B', 'BB': 'N', 'BC': 'B', 'CC': 'N', 'CN': 'C'}
+
+new_polymer = polymer
+for i in range(40):
+    new_polymer = insert(new_polymer)
+
+# count all the elements in the polymer
+count = Counter(new_polymer)
 # the answer
-print(max(count_polymer) - min(count_polymer))
-
-#====================================================================
+print(max(count.values()) - min(count.values()))
